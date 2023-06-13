3b1eaeb85d426c8da80340eacf0242f6c0be7b27
SofiaCurreri <sofiacurreri@gmail.com>
2023-06-13 11:17:50 +0200

    prenota() and disdici() in main class

---
+++ src/org/lessons/java/events/PrenotaDisdici.java
@@ -0,0 +1,69 @@
+package org.lessons.java.events;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class PrenotaDisdici {
+    public static void main(String[] args) {
+        Scanner utente = new Scanner(System.in);
+
+        System.out.println("Inserisci titolo dell' evento");
+        String titolo = utente.nextLine();
+
+        System.out.println("Inserisci data dell' evento (yy/MM/dd)");
+        LocalDate data = LocalDate.parse(utente.nextLine());
+
+        System.out.println("Inserisci il numero totale di posti dell' evento");
+        int postiTot = utente.nextInt();
+        utente.nextLine();
+
+        Evento evento = new Evento(titolo, data, postiTot);
+
+        boolean esci = false;
+        boolean esci2 = false;
+
+        //ciclo per prenotare
+        do{
+            System.out.println("Vuoi effettuare una o più prenotazioni? (s/n)");
+            String risposta = utente.nextLine().toLowerCase();
+            switch (risposta){
+                case "s":
+                    System.out.println("Quante prenotazioni vuoi effettuare?");
+                    int prenotazione = utente.nextInt();
+                    utente.nextLine();
+
+                    for (int i = 0; i < prenotazione; i++) {
+                        evento.prenota();
+                    }
+
+                    System.out.println("I posti prenotati totali sono: " + evento.getPostiPrenotati());
+                    System.out.println("I posti disponibili sono: " + (evento.getPostiTot() - evento.getPostiPrenotati()));
+                case "n":
+                    System.out.println("Arrivederci!");
+                    esci = true;
+            }
+        }while(!esci);
+
+        //ciclo per disdire
+        do{
+            System.out.println("Vuoi disdire una o più prenotazioni? (s/n)");
+            String risposta = utente.nextLine().toLowerCase();
+            switch (risposta){
+                case "s":
+                    System.out.println("Quante prenotazioni vuoi disdire?");
+                    int disdetta = utente.nextInt();
+                    utente.nextLine();
+
+                    for (int i = 0; i < disdetta; i++) {
+                        evento.disdici();
+                    }
+
+                    System.out.println("I posti prenotati totali sono: " + evento.getPostiPrenotati());
+                    System.out.println("I posti disponibili sono: " + (evento.getPostiTot() - evento.getPostiPrenotati()));
+                case "n":
+                    System.out.println("Arrivederci!");
+                    esci2 = true;
+            }
+        }while(!esci2);
+    }
+}
